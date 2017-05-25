6c161e521ebfbef596d179100dc480bdab6f461f
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-05-25 18:13:24 -0500

    modal working added jquer/bootstrap link to index.hb

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //click event to kick off the scraping
-    $('#scrape-news-button').on('click', function () {
-        $.get('/scrape', function (data) {
+    $('#scrape-news-button').on('click', function() {
+        $.get('/scrape', function(data) {
             location.reload();
         });
     });
 
 
-    var thisId;  //id of the current article
+    var thisId; //id of the current article
     //click event to add a note to the article
-    $('#addButton').on('click', function () {
-        $('#noteModal').modal('show');
+    $('#addButton').on('click', function() {
+        // $('#noteModal').modal('show');
         $('.note-div').empty();
         thisId = $(this).attr("data-value");
         $('.modal-title').html('Notes for Article: ' + thisId);
